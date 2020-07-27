528ff5628efb8533be95a80a658749a7f7019cbe
vvdolphin <1273027520@qq.com>
2020-07-27 22:22:03 +0800

    [guessNumber][Zoe]feature:game

--- src/main/java/example/Game.java
+++ src/main/java/example/Game.java
@@ -1,33 +1,47 @@
 package example;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Game {
     private GuessNumbers guessNumbers = new GuessNumbers();
-
-
-
-
+    private HashMap<String,String> results = new HashMap<>();
 
     public void game(){
-        int times = guessNumbers.getTimes();
-        for (int i = 0; i <times ; i++) {
+        int[] answer = guessNumbers.generateResult();
+        for (int i = 0; i <guessNumbers.getTimes() ; i++) {
+            int[] inputNumbers = getNumbers(i);
 
+            String isLegal = guessNumbers.isLegal(inputNumbers);
+            if ("000".equals(isLegal)){
+                String result = guessNumbers.checkResult(inputNumbers, answer);
+                if ("4A0B".equals(result)){
+                    System.out.println("guess right");
+                    break;
+                }else {
+                    results.put(inputNumbers.toString(),result);
+                    results.entrySet().stream().forEach(item->System.out.println(item.getKey()+" "+item.getValue()));
+                }
+            }else{
+                System.out.println("Wrong number,Input again");
+            }
         }
-
-
 
 
     }
 
-
-
-
-
-
+    private int[] getNumbers(int i) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.next().split(" ");
+        int[] inputNumbers= new int[input.length];
+        for (int j = 0; j <input.length ; j++) {
+            inputNumbers[i]=Integer.parseInt(input[i]);
+        }
+        return inputNumbers;
+    }
 
 
 }
