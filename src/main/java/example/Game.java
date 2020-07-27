9f51a0cb7a63b2f00c5ed1e18c5fb34ec5fde949
vvdolphin <1273027520@qq.com>
2020-07-27 22:38:21 +0800

    [guessNumber][Zoe]refactor:refactor method game

--- src/main/java/example/Game.java
+++ src/main/java/example/Game.java
@@ -9,39 +9,54 @@ import java.util.Scanner;
 public class Game {
     private GuessNumbers guessNumbers = new GuessNumbers();
     private HashMap<String,String> results = new HashMap<>();
+    private String correctAnswer ="4A0B";
+
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.game();
+    }
 
     public void game(){
         int[] answer = guessNumbers.generateResult();
-        for (int i = 0; i <guessNumbers.getTimes() ; i++) {
-            int[] inputNumbers = getNumbers(i);
-
-            String isLegal = guessNumbers.isLegal(inputNumbers);
-            if ("000".equals(isLegal)){
-                String result = guessNumbers.checkResult(inputNumbers, answer);
-                if ("4A0B".equals(result)){
-                    System.out.println("guess right");
-                    break;
-                }else {
-                    results.put(inputNumbers.toString(),result);
-                    results.entrySet().stream().forEach(item->System.out.println(item.getKey()+" "+item.getValue()));
-                }
-            }else{
-                System.out.println("Wrong number,Input again");
-            }
+        for (int i = 0; i <guessNumbers.getTimes(); i++) {
+            System.out.println("input number");
+            int[] inputNumbers = getNumbers();
+            guessNumber(inputNumbers, answer);
         }
-
-
     }
 
-    private int[] getNumbers(int i) {
+    private void guessNumber(int[] inputNumbers, int[] answer) {
+        if (isLegal(inputNumbers)){
+            printResult(inputNumbers,answer);
+        }else{
+            System.out.println("Wrong number,Input again");
+        }
+    }
+
+    private int[] getNumbers() {
         Scanner scanner = new Scanner(System.in);
-        String[] input = scanner.next().split(" ");
+        String[] input = scanner.nextLine().split(" ");
         int[] inputNumbers= new int[input.length];
-        for (int j = 0; j <input.length ; j++) {
+        for (int i = 0; i <input.length ; i++) {
             inputNumbers[i]=Integer.parseInt(input[i]);
         }
         return inputNumbers;
     }
 
+
+    private boolean isLegal(int[] inputNumbers){
+        return "000".equals(guessNumbers.isLegal(inputNumbers));
+    }
+
+    private void printResult(int[] inputNumbers,int[] answer){
+        String result = guessNumbers.checkResult(inputNumbers, answer);
+        if (correctAnswer.equals(result)){
+            System.out.println("guess right");
+        }else {
+            results.put(inputNumbers.toString(),result);
+            results.entrySet().stream().forEach(item->System.out.println(item.getKey()+" "+item.getValue()));
+        }
+    }
 
 }
